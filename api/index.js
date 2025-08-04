876ca06c5d206a4fe16999113219d3b36eaec0c1
clarissecasicarino <cclarissecasi@gmail.com>
2025-08-04 16:18:32 +0200

    Add cors in middleware

--- api/index.js
+++ api/index.js
@@ -7,7 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 3301;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://my-loot-bjgm.vercel.app"],
+  })
+);
 app.use(express.json());
 
 // Database connection
