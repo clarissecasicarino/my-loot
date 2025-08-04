99432f5c4aa737f05529b57e22d00380ae58983e
clarissecasicarino <cclarissecasi@gmail.com>
2025-08-04 16:22:51 +0200

    Change port to 3001

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3301;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(
